3703948a09f26bed5993e3c5d9fe5db4c2586557
Soroush-Mostafavi <soroush.mr1994@gmail.com>
2022-04-06 12:23:30 +0430

    add header.jsx

---
+++ src/Components/Header/Header.jsx
@@ -0,0 +1,71 @@
+import React from "react";
+
+
+  return (
+    <div className="text-center">
+      <h2 className="text-primary font-weight-bold mt-4">{t("cv.1")}</h2>
+      <h1 className="d-flex justify-content-center font-weight-bold ">
+       CV<h4>Builder</h4>
+      </h1>
+      <div className="mt-3">
+        {" "}
+       
+      </div>
+      <div className="d-flex">
+        <h4 style={{ width: "200px" }}>Language : </h4>
+        <button  className="btn">
+          <img
+            src="img/1200px-Flag_of_the_United_Kingdom.svg.png"
+            height="20px"
+            width="30px"
+            alt="gkughj"
+          />
+        </button>
+        <button  className="btn">
+          <img src="img/france.png" height="20px" width="30px" alt="gkughj"/>
+        </button>
+        <button  className="btn">
+          <img
+            src="img/2560px-Flag_of_the_United_Arab_Emirates_(3-2).svg.png"
+            height="20px"
+            width="30px"
+            alt="gkughj"
+          />
+        </button>
+        <button  className="btn">
+          <img
+            src="img/1200px-Flag_of_Spain.svg.png"
+            height="20px"
+            width="30px"
+            alt="gkughj"
+          />
+        </button>
+        <button  className="btn">
+          <img
+            src="img/2560px-Flag_of_Italy.svg.png"
+            height="20px"
+            width="30px"
+            alt="gkughj"
+          />
+        </button>
+        <button  className="btn">
+          <img src="img/download.jpg" alt="gkughj" height="20px" width="30px" />
+        </button>
+        <button  className="btn">
+          <img src="img/1280px-Flag_of_Russia.svg.png" alt="gkughj" height="20px" width="30px" />
+        </button>
+        <button  className="btn">
+          <img src="img/1280px-Flag_of_the_Netherlands.svg.png" alt="gkughj" height="20px" width="30px" />
+        </button>
+        <button  className="btn">
+          <img src="img/Flag_of_South_Korea.svg.png" alt="gkughj" height="20px" width="30px" />
+        </button>
+        <button  className="btn">
+          <img src="img/1200px-Flag_of_Germany.svg.png" alt="gkughj" height="20px" width="30px" />
+        </button>
+      </div>
+    </div>
+  );
+};
+
+export default Headerline;
