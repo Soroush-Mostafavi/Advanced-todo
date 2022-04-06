75c10131e4ddfb2719f1f715c8f62f0cf1040e14
Soroush-Mostafavi <soroush.mr1994@gmail.com>
2022-04-06 17:12:10 +0430

    add toggle mode/name/theme to file

--- src/Components/Main/Main.jsx
+++ src/Components/Main/Main.jsx
@@ -1,16 +1,20 @@
 import React, { Fragment } from "react";
-const MainMode = () => {
+const MainMode = ({
+    toggleMode,
+    toggleName,
+    toggleTheme
+}) => {
   return (
     <Fragment>
       {" "}<div className="container mt-2 d-flex">
       <div className="mt-4 mr-3" style={{ marginTop: "15px" }}>
-       
+        {toggleName()}
       </div>
       <label class="switch mt-4" style={{ marginTop: "25px" }}>
-        <input type="checkbox" />
+        <input type="checkbox" onClick={toggleTheme} />
         <span class="slider round"></span>
       </label>
-      <div className="mt-3 ml-4"></div></div>
+      <div className="mt-3 ml-4">{toggleMode()}</div></div>
     </Fragment>
   );
 };
